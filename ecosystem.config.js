3243f4f6af116b35cba5313ea9b8c1ea9a2b8eab
Sha_code <shannone.controu@gmail.com>
2020-03-05 10:30:11 +0100

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [{
+    name: 'BuyTheWay_API',
+    script: 'server.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      "key": "./awsEc2.pem",
+      user: 'ubuntu',
+      host: 'ec2-54-164-43-47.compute-1.amazonaws.com',
+      ref: 'origin/master',
+      repo: 'git@github.com:O-clock-X/BuyTheWay-Back.git',
+      path: '/home/ubuntu',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
