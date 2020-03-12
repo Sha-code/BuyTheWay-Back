0bcb50bd29047e2044799bda00638162e10bdd57
Sha_code <shannone.controu@gmail.com>
2020-03-12 16:52:56 +0100

    sku protected change

--- routes/skuRoutes.js
+++ routes/skuRoutes.js
@@ -5,9 +5,9 @@ const checkAuth = require('../middleware/check-auth');
 const router = express.Router();
 
 
-router.use(checkAuth);
 router.get('/sku/:sid', skuControllers.getSkuById)
 router.get('/skus-by-product/:pid', skuControllers.getSkuByProductId)
+router.use(checkAuth);
 router.post('/sku/add', skuControllers.addNewSku)
 router.post('/sku/update-one/:sid', skuControllers.updatedSku)
 router.delete('/sku/remove-all/:pid', skuControllers.removeSkuByProductId)
