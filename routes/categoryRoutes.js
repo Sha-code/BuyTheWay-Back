07a4800da2b6240b6d81fa40c070a189c4d52b0a
Sha_code <shannone.controu@gmail.com>
2020-03-12 17:11:56 +0100

    user prtected change

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -6,17 +6,18 @@ const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
-router.get('/gender/:gender', categoriesControllers.getCategoryByGender)
+router.get('/gender/:gender', categoriesControllers.getCategoryByGender);
+
 router.use(checkAuth);
-router.post('/category/add',[
+router.post('/category/add', [
     check('name')
         .not()
         .isEmpty(),
     check('gender')
         .not()
         .isEmpty(),
-], categoriesControllers.addNewCategory)
-router.post('/category/update/:cid', categoriesControllers.updatedCategory)
-router.delete('/category/:cid', categoriesControllers.deletedCategory)
+], categoriesControllers.addNewCategory);
+router.post('/category/update/:cid', categoriesControllers.updatedCategory);
+router.delete('/category/:cid', categoriesControllers.deletedCategory);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,34 +5,35 @@ const usersControllers = require('../controllers/users-controllers');
 const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
+router.post('/user/login', usersControllers.login);
+
+router.post(
+  '/user/signup',
+  [
+    check('nickname')
+      .not()
+      .isEmpty(),
+    check('mail')
+      .normalizeEmail()
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersControllers.signup
+);
+
+router.post('/user/update/:uid',
+  [
+    check('nickname')
+      .not()
+      .isEmpty(),
+    check('mail')
+      .normalizeEmail()
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ], usersControllers.updatedUser)
 
 router.use(checkAuth);
 router.get('/user/:uid', usersControllers.getUserById)
-router.post(
-    '/user/signup',
-    [
-      check('nickname')
-        .not()
-        .isEmpty(),
-      check('mail')
-        .normalizeEmail()
-        .isEmail(),
-      check('password').isLength({ min: 6 })
-    ],
-    usersControllers.signup
-  );
-
-router.post('/user/update/:uid',
-[
-    check('nickname')
-        .not()
-        .isEmpty(),
-    check('mail')  
-        .normalizeEmail()
-        .isEmail(),
-    check('password').isLength({ min:6 })
-], usersControllers.updatedUser)
-router.post('/user/login', usersControllers.login);
 router.delete('/user/remove/:uid', usersControllers.removeUserById)
 
 
