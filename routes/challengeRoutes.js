93bc46399ababf3d7b0a3225b87bf8efad0f1648
Sha_code <shannone.controu@gmail.com>
2020-03-16 09:52:08 +0100

    remove check auth

--- routes/challengeRoutes.js
+++ routes/challengeRoutes.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const { check } = require('express-validator')
 const challengesControllers = require('../controllers/challenges-controllers');
-const checkAuth = require('../middleware/check-auth');
+// const checkAuth = require('../middleware/check-auth');
 
 
 const router = express.Router();
 
 
 router.get('/challenge/:cid', challengesControllers.getChallengeById)
-router.use(checkAuth);
-router.post('/challenge/add',[
-    check('title','fidelity_gain','date_start','date_end','tag')
+// router.use(checkAuth);
+router.post('/challenge/add', [
+    check('title', 'fidelity_gain', 'date_start', 'date_end', 'tag')
         .not()
         .isEmpty(),
-    check('description').isLength({min: 5}),
-    
+    check('description').isLength({ min: 5 }),
+
 ], challengesControllers.addNewChallenge)
 router.post('/challenge/update/:cid',
-[
-    check('title','fidelity_gain','date_start','date_end','tag')
-        .not()
-        .isEmpty(),
-    check('description').isLength({min: 5}),
-    
-], challengesControllers.updatedChallenge)
+    [
+        check('title', 'fidelity_gain', 'date_start', 'date_end', 'tag')
+            .not()
+            .isEmpty(),
+        check('description').isLength({ min: 5 }),
+
+    ], challengesControllers.updatedChallenge)
 router.delete('/challenge/:cid', challengesControllers.deletedChallenge)
 
 
